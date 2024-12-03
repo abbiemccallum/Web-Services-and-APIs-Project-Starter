590c5f5513380da4239ceff570cdb54016abeb5e
abbiemccallum <abbiemccallum@hotmail.co.uk>
2024-12-03 16:18:45 +0000

    Adding Swagger Documentation

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -1,7 +1,5 @@
 package com.udacity.pricing;
 
-import com.udacity.pricing.service.PricingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
